222a691fd736eaa2249a4a81d25fa1417997e6f4
Vinicius <andradevinihenrique@gmail.com>
2024-12-09 16:29:42 -0300

    Alterações feitas! Home-Sobrenos

--- src/@components/SobreNos/SobreNos.tsx
+++ src/@components/SobreNos/SobreNos.tsx
@@ -5,36 +5,30 @@ export const SobreNos = () => (
 		<h2>Conheça um pouco mais sobre a TechAcademy</h2>
 		<div className="row">
 			<div className="col-md-4">
-				<div className="info">
-					<div className="conteudo-info">
-						<div className="numero-conteudo">
-								1
-						</div>
-						<h5>Kursus Gratis</h5>
-						<p>Kami menyediakan beberapa kursus gratis untuk menunjang ketersediaan pendidikan untuk masyarakat kurang mampu.</p>
-					</div>				
-				</div>
-			</div>
-			<div className="col-md-4">
-				<div className="info">
-					<div className="conteudo-info">
-						<div className="numero-conteudo">
-								2
-						</div>
-						<h5>Akses Selamanya</h5>
-						<p>Semua kursus yang telah kamu daftar bisa diakses selamanya sehingga belajar kamu lebih nyaman dan tidak terburu-buru. </p>
-					</div>								
-				</div>
-			</div>
-			<div className="col-md-4">
-				<div className="info">
-					<div className="conteudo-info">
-						<div className="numero-conteudo">
-								3
-						</div>
-						<h5>Grup Konsultasi</h5>
-						<p>Terdapat grup konsultasi yang berguna jika kamu ingin menanyakan suatu pertanyaan  dan kamu juga bisa mebuka diskusi baru.</p>
+				<div className="conteudo-info">
+					<div className="numero-conteudo">
+							1
 					</div>
+					<h5>Kursus Gratis</h5>
+					<p>Kami menyediakan beberapa kursus gratis untuk menunjang ketersediaan pendidikan untuk masyarakat kurang mampu.</p>
+				</div>				
+			</div>
+			<div className="col-md-4">
+				<div className="conteudo-info">
+					<div className="numero-conteudo">
+							2
+					</div>
+					<h5>Akses Selamanya</h5>
+					<p>Semua kursus yang telah kamu daftar bisa diakses selamanya sehingga belajar kamu lebih nyaman dan tidak terburu-buru. </p>
+				</div>								
+			</div>
+			<div className="col-md-4">
+				<div className="conteudo-info">
+					<div className="numero-conteudo">
+								3
+					</div>
+					<h5>Grup Konsultasi</h5>
+					<p>Terdapat grup konsultasi yang berguna jika kamu ingin menanyakan suatu pertanyaan  dan kamu juga bisa mebuka diskusi baru.</p>
 				</div>
 			</div>			
 		</div>		
