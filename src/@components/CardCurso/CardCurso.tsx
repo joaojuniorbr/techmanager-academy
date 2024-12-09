1c55285c8fd36ff123cc8f0b701c8a418f5db7fe
João Junior <negrelis@gmail.com>
2024-12-09 20:05:52 -0300

    :sparkles: animacao no mouse hover nos cursos

--- src/@components/CardCurso/CardCurso.tsx
+++ src/@components/CardCurso/CardCurso.tsx
@@ -2,37 +2,48 @@ import dayjs from 'dayjs';
 import { CursoInterface } from '../../@hooks';
 
 import './CardCurso.css';
+import { message } from 'antd';
 
-export const CardCurso = ({ item }: { item: CursoInterface }) => (
-	<div className='card-curso'>
-		<div className='card-curso--image'>
-			<img src={item.image} alt={item.title} />
+export const CardCurso = ({ item }: { item: CursoInterface }) => {
+	const handleClick = (event: React.MouseEvent) => {
+		event.preventDefault();
+		message.success('Esta página ainda está em desenvolvimento');
+	};
 
-			<div className='cart-curso--rate'>
-				<i className='ri-star-fill'></i>
-				{item.rate}
-			</div>
-		</div>
+	return (
+		<div className='card-curso'>
+			<div className='card-curso--image'>
+				<a href='#' onClick={handleClick}>
+					<span>Saiba Mais sobre o Curso</span>
+					<img src={item.image} alt={item.title} />
+				</a>
 
-		<div className='card-curso--description'>
-			<h3>{item.title}</h3>
-			<p>{item.description}</p>
-
-			<div className='row'>
-				<div className='col'>
-					<div className='card-curso--highlight'>
-						<i className='ri-time-line'></i>
-						{dayjs(item.datetime).format('DD/MM/YYYY')}
-					</div>
-				</div>
-				<div className='col-auto'>
-					<div className='card-curso--highlight'>
-						<i className='ri-play-circle-line'></i>
-						{item.videos}
-						{item.videos > 1 ? ' Aulas' : ' Aula'}
-					</div>
+				<div className='cart-curso--rate'>
+					<i className='ri-star-fill'></i>
+					{item.rate}
 				</div>
 			</div>
+
+			<div className='card-curso--description'>
+				<h3>{item.title}</h3>
+				<p>{item.description}</p>
+
+				<div className='row'>
+					<div className='col'>
+						<div className='card-curso--highlight'>
+							<i className='ri-time-line'></i>
+							{dayjs(item.datetime).format('DD/MM/YYYY')}
+						</div>
+					</div>
+					<div className='col-auto'>
+						<div className='card-curso--highlight'>
+							<i className='ri-play-circle-line'></i>
+							{item.videos}
+							{item.videos > 1 ? ' Aulas' : ' Aula'}
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
-	</div>
-);
+	);
+};
