31d8393e8cb68472647d1c68cc6edcbc553aca46
João Junior <negrelis@gmail.com>
2024-12-09 18:23:41 -0300

    :sparkles: titulo dos cursos, com padronização de titulo e botao

---
+++ src/@components/CardCurso/CardCurso.tsx
@@ -0,0 +1,11 @@
+export const CardCurso = () => {
+	return (
+		<div className='card'>
+			<div className='card__header'>
+				<img src='' alt='' />
+			</div>
+			<div className='card__body'></div>
+			<div className='card__footer'></div>
+		</div>
+	);
+};
--- src/@components/Home/Cursos/Cursos.tsx
+++ src/@components/Home/Cursos/Cursos.tsx
@@ -1,13 +1,28 @@
 import { useCursos } from '../../../@hooks';
 
+import './Cursos.css';
+
 export const Cursos = () => {
 	const { data } = useCursos();
 
 	console.log(data);
 
 	return (
-		<section>
-			<div className='container'>Cursos</div>
+		<section className='home-cursos'>
+			<div className='container'>
+				<div className='home-cursos--titulo'>
+					<div className='row align-items-center'>
+						<div className='col'>
+							<h2 className='title-default'>Nossos Cursos</h2>
+						</div>
+						<div className='col-auto'>
+							<a href='/cursos' className='button-default'>
+								Todos os Cursos
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
 		</section>
 	);
 };
