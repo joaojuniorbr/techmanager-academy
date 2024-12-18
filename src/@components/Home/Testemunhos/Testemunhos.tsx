b18aa60f077d0fce1b527470653ce9603768234e
Vinicius <andradevinihenrique@gmail.com>
2024-12-18 20:26:06 -0300

    Home-Testemunhos feito (sem carrosel)

--- src/@components/Home/Testemunhos/Testemunhos.tsx
+++ src/@components/Home/Testemunhos/Testemunhos.tsx
@@ -1,36 +1,36 @@
 import "./Testemunhos.css";
 
 export const Testemunhos = () => (
-	<section className="testemunho">
-		<div className="container pt-5">
-			<div className="row justify-content-center">
-				<div className="col-lg-4 col-4">
-					<h3>O que falam sobre nós?</h3>
-					<p className="text-secondary">Mais de 20.000 usuários</p>
-				</div>
-				<div className="col-lg-7">
-					<p className="fala-testemunho">
-						"Materi yang disampaikan mudah dimengerti, kualitas instruktur sangat baik serta
-						responnya yang cepat. So, kursus disini sangat sangat saya rekomendasikan!!"
-					</p>
+		<section className="testemunho">
+			<div className="container">
+				<div className="row justify-content-center">
+					<div className="col-lg-4 chamada-testemunho">
+						<h3>O que falam sobre nós?</h3>
+						<p className="text-secondary">Mais de 20.000 usuários</p>
+					</div>
+					<div className="col-lg-7">
+						<p className="fala-testemunho">
+							"Materi yang disampaikan mudah dimengerti, kualitas instruktur sangat baik serta
+							responnya yang cepat. So, kursus disini sangat sangat saya rekomendasikan!!"
+						</p>
 
-					<div className="row align-items-center mt-3">
-						<div className="col-lg-1">
-							<div className="rounded-circle foto-testemunho"></div>
-						</div>							
-						<div className="col-lg-3 align-self-center">
-							<span className="fw-bold">Jenny Wilson</span>
-							<p className="text-secondary mb-0">Vice President</p>
-						</div>
-						<div className="col-lg d-flex justify-content-end">
-							<div className="rounded-circle lista-testemunho mx-1 ativo"></div>
-							<div className="rounded-circle lista-testemunho mx-1"></div>
-							<div className="rounded-circle lista-testemunho mx-1"></div>
-							<div className="rounded-circle lista-testemunho mx-1"></div>
+						<div className="row align-items-center">
+							<div className="col-lg-auto">
+								<img src="/Imagem_pessoa.png" className="rounded-circle foto-testemunho"/>
+							</div>							
+							<div className="col-lg align-self-center">
+								<span className="fw-bold">Jenny Wilson</span>
+								<p className="text-secondary mb-0">Vice President</p>
+							</div>
+							<div className="col-lg-auto">
+								<div className="rounded-circle lista-testemunho mx-1 ativo"></div>
+								<div className="rounded-circle lista-testemunho mx-1"></div>
+								<div className="rounded-circle lista-testemunho mx-1"></div>
+								<div className="rounded-circle lista-testemunho mx-1"></div>
+							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
-	</section>
+		</section>
 );
