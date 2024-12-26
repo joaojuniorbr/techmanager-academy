5fdffec1af8c844b20aa31ee421bc6544c1fb291
João Junior <negrelis@gmail.com>
2024-12-26 09:43:27 -0300

    :sparkles: conteúdo sobre nos na home

--- src/@components/Home/SobreNos/HomeSobreNos.tsx
+++ src/@components/Home/SobreNos/HomeSobreNos.tsx
@@ -1,36 +1,42 @@
-import "./HomeSobreNos.css";
+import './HomeSobreNos.css';
 
 export const SobreNos = () => (
-	<section className="container">
+	<section className='container'>
 		<h2>Conheça um pouco mais sobre a TechAcademy</h2>
-		<div className="row">
-			<div className="col-md-4">
-				<div className="conteudo-info">
-					<div className="numero-conteudo">
-							1
-					</div>
-					<h5>Kursus Gratis</h5>
-					<p>Kami menyediakan beberapa kursus gratis untuk menunjang ketersediaan pendidikan untuk masyarakat kurang mampu.</p>
-				</div>				
-			</div>
-			<div className="col-md-4">
-				<div className="conteudo-info">
-					<div className="numero-conteudo">
-							2
-					</div>
-					<h5>Akses Selamanya</h5>
-					<p>Semua kursus yang telah kamu daftar bisa diakses selamanya sehingga belajar kamu lebih nyaman dan tidak terburu-buru. </p>
-				</div>								
-			</div>
-			<div className="col-md-4">
-				<div className="conteudo-info">
-					<div className="numero-conteudo">
-								3
-					</div>
-					<h5>Grup Konsultasi</h5>
-					<p>Terdapat grup konsultasi yang berguna jika kamu ingin menanyakan suatu pertanyaan  dan kamu juga bisa mebuka diskusi baru.</p>
+		<div className='row'>
+			<div className='col-md-4'>
+				<div className='conteudo-info'>
+					<div className='numero-conteudo'>1</div>
+					<h5>Cursos Gratuitos</h5>
+					<p>
+						Disponibilizamos uma variedade de cursos gratuitos, projetados para
+						capacitar líderes e profissionais na área de tecnologia, tornando o
+						conhecimento acessível a todos.
+					</p>
 				</div>
-			</div>			
-		</div>		
+			</div>
+			<div className='col-md-4'>
+				<div className='conteudo-info'>
+					<div className='numero-conteudo'>2</div>
+					<h5>Acesso Vitalício</h5>
+					<p>
+						Todos os cursos oferecidos podem ser acessados a qualquer momento,
+						garantindo que você aprenda no seu ritmo, sem pressa e com
+						tranquilidade.
+					</p>
+				</div>
+			</div>
+			<div className='col-md-4'>
+				<div className='conteudo-info'>
+					<div className='numero-conteudo'>3</div>
+					<h5>Comunidade e Consultoria</h5>
+					<p>
+						Participe de grupos de discussão e mentorias exclusivas, onde você
+						pode tirar dúvidas, compartilhar ideias e colaborar com outros
+						profissionais da área.
+					</p>
+				</div>
+			</div>
+		</div>
 	</section>
 );
