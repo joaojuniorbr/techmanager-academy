343bcfae9d7cdd47a5a34e59c9a749ea95d3d061
Joao Junior <negrelis@gmail.com>
2024-12-26 23:51:29 -0300

    Atualização de Conteúdo (#31)
    * :sparkles: adicionado o conteúdo no hero da home
    
    * :sparkles: conteúdo sobre nos na home
    
    * :bug: alteração da logo principal

--- src/@components/Home/Hero/Hero.tsx
+++ src/@components/Home/Hero/Hero.tsx
@@ -6,24 +6,26 @@ export const Hero = () => (
 			<div className='row align-items-center'>
 				<div className='col-xxl-5 col-xl-6 col-lg-7'>
 					<article>
-						<h2>Bangun dan Wujudkan Cita Bersama EDUFREE</h2>
+						<h2>Construa e Realize Seus Objetivos com o TechManager Academy</h2>
 
 						<p>
-							EDUFREE adalah sebuah layanan kursus dan pelatihan gratis secara
-							online yang bertujuan untuk membantu kamu meraih cita di bidang
-							teknologi.
+							O TechManager Academy é uma plataforma de cursos e treinamentos
+							online gratuitos, projetada para capacitar líderes e gestores a
+							alcançarem o sucesso no setor de tecnologia.
 						</p>
 					</article>
 
 					<nav className='row'>
 						<div className='col-6 col-md-auto col-lg-6'>
-							<button className='button-default'>Lihat Kursus</button>
+							<a href='/cursos' className='button-default'>
+								Ver Cursos
+							</a>
 						</div>
 						<div className='col-6 col-md-auto col-lg-6'>
-							<button className='button-default transparent'>
-								Lihat Alur Belajar
+							<a href='/sobre-nos' className='button-default transparent'>
+								Saiba Mais
 								<i className='ri-arrow-right-line'></i>
-							</button>
+							</a>
 						</div>
 					</nav>
 				</div>
