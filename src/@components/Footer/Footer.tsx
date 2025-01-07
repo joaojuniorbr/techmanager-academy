9022f391735d97aedbbc2a9d1eaff1f3cf29fab4
Patrick Domênico dos Santos Pereira Alves <patrickdomeni61@gmail.com>
2025-01-07 23:32:57 +0000

    ajustes

--- src/@components/Footer/Footer.tsx
+++ src/@components/Footer/Footer.tsx
@@ -2,46 +2,46 @@ import './Footer.css';
 
 export const Footer = () => {
 	return <div>
-<footer>
-      <section style="background-color: #1A1B44;">
-        <div class="container text-white">
-          <div class="row p-5">
-            <div class="col-7">
-              <h4 class="mb-3" style="font-weight: 700;">TechAcademy</h4>
-              <p>Bangun dan wujudkan cita bersama edufree</p>
+  <footer>
+    <section className='footer'>
+        <div className="container text-white">
+          <div className="row p-5">
+            <div className="col-lg-7 mb-5">
+              <h4 className="mb-3" >TechManager Academy</h4>
+              <p>Construa e Realize seus objetivos!</p>
             </div>
-            <div class="col-3">
-              <h5 class="mb-3">Redes Sociais</h5>
-              <nav>
-                <a href="#"><i class='ri-instagram-line'></i></a>
-                <a href="#"><i class='ri-facebook-box-line'></i></a>
-                <a href="#"><i class='ri-twitter-x-line'></i></a>
+            <div className="col-lg-3 mb-5">
+              <h5 className="mb-3">Redes Sociais</h5>
+              <nav className='social-nav'>
+                <a href="#"><i className='ri-instagram-line'></i></a>
+                <a href="#"><i className='ri-facebook-box-line'></i></a>
+                <a href="#"><i className='ri-twitter-x-line'></i></a>
               </nav>
             </div>
-            <div class="col">
-              <h5 class="mb-3">Acesse</h5>
-              <ul class="nav flex-column">
-                <li class="nav-item mb-2"><a href="#" class="text-white" style="text-decoration: none;">Home</a></li>
-                <li class="nav-item mb-2"><a href="#" class="text-white" style="text-decoration: none;">Sobre Nós</a></li>
-                <li class="nav-item mb-2"><a href="#" class="text-white" style="text-decoration: none;">Cursos</a></li>
-                <li class="nav-item mb-4"><a href="#" class="text-white" style="text-decoration: none;">Contato</a></li>
+            <div className="col">
+              <h5 className="mb-3">Acesse</h5>
+              <ul className="nav flex-column sobrenos-ul" >
+                <li className="nav-item mb-2"><a href="/">Home</a></li>
+                <li className="nav-item mb-2"><a href="/sobre-nos">Sobre Nós</a></li>
+                <li className="nav-item mb-2"><a href="/cursos">Cursos</a></li>
+                <li className="nav-item mb-4"><a href="/contato">Contato</a></li>
               </ul>
             </div>
           </div>
         </div>
       </section>
       <section>
-        <div class="container text-black m-auto p-5 pt-3 pb-0 bg-white">
-          <div class="row p-3">
-            <div class="col">
-              <p>TechAcademy© 2024</p>
+        <div className="container text-black m-auto p-5 pt-3 pb-0 bg-white">
+          <div className="row p-3">
+            <div className="col">
+              <p>TechManager Academy © 2024</p>
             </div>
-            <div class="col text-end">
-              <a href="#" class="text-black" style="text-decoration: none;">Política de Privacidade</a>
+            <div className="col text-end politica-div">
+              <a href="#" className="text-black">Política de Privacidade</a>
             </div>
           </div>
          </div>
       </section>
-    </footer>
+  </footer>
 </div>;
 };
