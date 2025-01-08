e7337590ded421a1cab4a4fff32eac1ffc06d842
João Junior <negrelis@gmail.com>
2025-01-07 21:25:44 -0300

    :sparkles: menu mobile

--- src/@components/Contato/Contato.tsx
+++ src/@components/Contato/Contato.tsx
@@ -2,78 +2,98 @@ import './Contato.css';
 import { Form, Input } from 'antd';
 
 export const Contato = () => {
-  const onFinish = (values:{
-    nome:string;
-    email:string;
-    telefone:string;
-  }) => {
-    const { nome, email, telefone } = values;
-    const mensagem = `Olá! Meu nome é ${nome}, meu e-mail é ${email}, e meu telefone é ${telefone}`;
-    window.open(`https://wa.me/5551999999999?text=${encodeURIComponent(mensagem)}`);
-  };
+	const onFinish = (values: {
+		nome: string;
+		email: string;
+		telefone: string;
+	}) => {
+		const { nome, email, telefone } = values;
+		const mensagem = `Olá! Meu nome é ${nome}, meu e-mail é ${email}, e meu telefone é ${telefone}`;
+		window.open(
+			`https://wa.me/5551999999999?text=${encodeURIComponent(mensagem)}`
+		);
+	};
 
-  return (
-    <section className='container'>
-      <div className='row'>
-        <div className='contato-conteudo'>
-          <div className='col-lg-7'>
-            <h4>Fale conosco</h4>
-            <h1>Contato</h1>
-            <div className='conteudo-texto'>
-              <p>
-                Entre em contato conosco! Aqui na <b>TechManager Academy</b>, acreditamos que a educação é a chave para o crescimento e a transformação. Estamos comprometidos em oferecer a melhor experiência de aprendizado possível e teremos o prazer em ajudá-lo(a) em sua jornada. Sua evolução é a nossa prioridade!
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className='row g-0'>
-        <img src='/Imagem_Contato.png' alt='CONTATO' className='col-lg-6' />
-        <div className='col-lg-6 form-contato'>
-          <div className='form-conteudo'>
-            <h3>Entre em Contato</h3>
-            <p>Preencha abaixo o formulário que logo entraremos em contato</p>
-            <Form
-              layout='vertical'
-              onFinish={onFinish}
-              requiredMark={false}
-            >
-              <Form.Item
-                name='nome'
-                rules={[
-                  { required: true, message: 'Por favor, insira seu nome!' },
-                ]}
-              >
-                <Input placeholder='Seu nome' className='custom-input' type='text'/>
-              </Form.Item>
+	return (
+		<section className='container mb-5'>
+			<div className='row'>
+				<div className='contato-conteudo'>
+					<div className='col-lg-7'>
+						<h4>Fale conosco</h4>
+						<h1>Contato</h1>
+						<div className='conteudo-texto'>
+							<p>
+								Entre em contato conosco! Aqui na <b>TechManager Academy</b>,
+								acreditamos que a educação é a chave para o crescimento e a
+								transformação. Estamos comprometidos em oferecer a melhor
+								experiência de aprendizado possível e teremos o prazer em
+								ajudá-lo(a) em sua jornada. Sua evolução é a nossa prioridade!
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className='row g-0'>
+				<img src='/Imagem_Contato.png' alt='CONTATO' className='col-lg-6' />
+				<div className='col-lg-6 form-contato'>
+					<div className='form-conteudo'>
+						<h3>Entre em Contato</h3>
+						<p>Preencha abaixo o formulário que logo entraremos em contato</p>
+						<Form layout='vertical' onFinish={onFinish} requiredMark={false}>
+							<Form.Item
+								name='nome'
+								rules={[
+									{ required: true, message: 'Por favor, insira seu nome!' },
+								]}
+							>
+								<Input
+									placeholder='Seu nome'
+									className='custom-input'
+									type='text'
+								/>
+							</Form.Item>
 
-              <Form.Item
-                name='email'
-                rules={[
-                  { required: true, message: 'Por favor, insira seu e-mail!' },
-                  { type: 'email', message: 'Por favor, insira um e-mail válido!' },
-                ]}
-              >
-                <Input placeholder='Seu e-mail' className='custom-input' type='email'/>
-              </Form.Item>
+							<Form.Item
+								name='email'
+								rules={[
+									{ required: true, message: 'Por favor, insira seu e-mail!' },
+									{
+										type: 'email',
+										message: 'Por favor, insira um e-mail válido!',
+									},
+								]}
+							>
+								<Input
+									placeholder='Seu e-mail'
+									className='custom-input'
+									type='email'
+								/>
+							</Form.Item>
 
-              <Form.Item
-                name='telefone'
-                rules={[
-                  { required: true, message: 'Por favor, insira seu telefone!' },
-                  { pattern: /^\d+$/, message: 'Digite apenas números!' },
-                ]}
-              >
-                <Input placeholder='Seu telefone' className='custom-input' type='text'/>
-              </Form.Item>
+							<Form.Item
+								name='telefone'
+								rules={[
+									{
+										required: true,
+										message: 'Por favor, insira seu telefone!',
+									},
+									{ pattern: /^\d+$/, message: 'Digite apenas números!' },
+								]}
+							>
+								<Input
+									placeholder='Seu telefone'
+									className='custom-input'
+									type='text'
+								/>
+							</Form.Item>
 
-              <button type='submit' className='botao-form'>
-                ENVIAR
-              </button>
-            </Form>
-          </div>
-        </div>
-      </div>
-    </section>
-  );
+							<button type='submit' className='botao-form'>
+								ENVIAR
+							</button>
+						</Form>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
 };
--- src/@components/Header/Header.tsx
+++ src/@components/Header/Header.tsx
@@ -1,30 +1,53 @@
 import { Link } from 'react-router-dom';
 import './Header.css';
+import { Drawer } from 'antd';
+import { useState } from 'react';
 
-export const Header = () => (
-	<header className='header'>
-		<div className='container'>
-			<nav className='row align-items-center'>
-				<div className='col'>
-					<div className='logo'>
-						<Link to='/'>
-							<img src='/logo-main-white.svg' alt='TechManager Academy' />
-						</Link>
-					</div>
-				</div>
-				<div className='col-auto'>
-					<div className='navigation d-none d-md-flex'>
-						<Link to='/'>HOME</Link>
-						<Link to='/sobre-nos'>SOBRE NÓS</Link>
-						<Link to='/cursos'>CURSOS</Link>
-						<Link to='/contato'>CONTATO</Link>
-					</div>
+export const Header = () => {
+	const [open, setOpen] = useState(false);
 
-					<div className='button-mobile d-flex d-md-none'>
-						<i className='ri-menu-line'></i>
-					</div>
+	const toggleMenu = () => {
+		setOpen(!open);
+	};
+
+	return (
+		<>
+			<header className='header'>
+				<div className='container'>
+					<nav className='row align-items-center'>
+						<div className='col'>
+							<div className='logo'>
+								<Link to='/'>
+									<img src='/logo-main-white.svg' alt='TechManager Academy' />
+								</Link>
+							</div>
+						</div>
+						<div className='col-auto'>
+							<div className='navigation d-none d-md-flex'>
+								<Link to='/'>HOME</Link>
+								<Link to='/sobre-nos'>SOBRE NÓS</Link>
+								<Link to='/cursos'>CURSOS</Link>
+								<Link to='/contato'>CONTATO</Link>
+							</div>
+
+							<div
+								className='button-mobile d-flex d-md-none'
+								onClick={toggleMenu}
+							>
+								<i className='ri-menu-line' />
+							</div>
+						</div>
+					</nav>
 				</div>
-			</nav>
-		</div>
-	</header>
-);
+			</header>
+			<Drawer open={open} title='Menu Principal' onClose={toggleMenu}>
+				<nav className='nav-mobile'>
+					<Link to='/'>HOME</Link>
+					<Link to='/sobre-nos'>SOBRE NÓS</Link>
+					<Link to='/cursos'>CURSOS</Link>
+					<Link to='/contato'>CONTATO</Link>
+				</nav>
+			</Drawer>
+		</>
+	);
+};
