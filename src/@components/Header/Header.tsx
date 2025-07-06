ad1a556565269814834eacb85dc68b294c95b32f
viniciusAhenrique <andradevinihenrique@gmail.com>
2025-07-06 17:56:29 -0300

    Botão de login adicionado ao responsivo

--- src/@components/Header/Header.tsx
+++ src/@components/Header/Header.tsx
@@ -1,90 +1,93 @@
-import { Link } from 'react-router-dom';
-import './Header.css';
-import { Button, Drawer, Spin } from 'antd';
-import { useState } from 'react';
-import { FormLogin } from '../FormLogin';
-import { useAuth } from '@hooks/useAuth';
-import { FormProfile } from '@components/FormProfile';
+import { Link } from "react-router-dom";
+import "./Header.css";
+import { Button, Drawer, Spin } from "antd";
+import { useState } from "react";
+import { FormLogin } from "../FormLogin";
+import { useAuth } from "@hooks/useAuth";
+import { FormProfile } from "@components/FormProfile";
 
 export const Header = () => {
-	const { data: profile, isLoading, logout } = useAuth();
+  const { data: profile, isLoading, logout } = useAuth();
 
-	const [open, setOpen] = useState(false);
-	const [openLogin, setOpenLogin] = useState(false);
-	const [isLoadingLogout, setIsLoadingLogout] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [openLogin, setOpenLogin] = useState(false);
+  const [isLoadingLogout, setIsLoadingLogout] = useState(false);
 
-	const toggleMenu = () => {
-		setOpen(!open);
-	};
+  const toggleMenu = () => {
+    setOpen(!open);
+  };
 
-	const toggleLogin = () => {
-		setOpenLogin(!openLogin);
-	};
+  const toggleLogin = () => {
+    setOpenLogin(!openLogin);
+  };
 
-	const labelAuth = profile?.data ? 'Perfil' : 'Login';
+  const labelAuth = profile?.data ? "Perfil" : "Login";
 
-	const handleLogout = () => {
-		setIsLoadingLogout(true);
-		logout().finally(() => setIsLoadingLogout(false));
-	};
+  const handleLogout = () => {
+    setIsLoadingLogout(true);
+    logout().finally(() => setIsLoadingLogout(false));
+  };
 
-	return (
-		<Spin spinning={isLoadingLogout || isLoading}>
-			<header className='header'>
-				<div className='container'>
-					<nav className='row align-items-center'>
-						<div className='col'>
-							<div className='logo'>
-								<Link to='/'>
-									<img src='/logo-main-white.svg' alt='TechManager Academy' />
-								</Link>
-							</div>
-						</div>
-						<div className='col-auto'>
-							<div className='navigation d-none d-md-flex'>
-								<Link to='/'>HOME</Link>
-								<Link to='/sobre-nos'>SOBRE NÓS</Link>
-								<Link to='/cursos'>CURSOS</Link>
-								<Link to='/contato'>CONTATO</Link>
+  return (
+    <Spin spinning={isLoadingLogout || isLoading}>
+      <header className="header">
+        <div className="container">
+          <nav className="row align-items-center">
+            <div className="col">
+              <div className="logo">
+                <Link to="/">
+                  <img src="/logo-main-white.svg" alt="TechManager Academy" />
+                </Link>
+              </div>
+            </div>
+            <div className="col-auto">
+              <div className="navigation d-none d-md-flex">
+                <Link to="/">HOME</Link>
+                <Link to="/sobre-nos">SOBRE NÓS</Link>
+                <Link to="/cursos">CURSOS</Link>
+                <Link to="/contato">CONTATO</Link>
 
-								<button className='button-default' onClick={toggleLogin}>
-									{labelAuth}
-								</button>
-							</div>
+                <button className="button-default" onClick={toggleLogin}>
+                  {labelAuth}
+                </button>
+              </div>
 
-							<div
-								className='button-mobile d-flex d-md-none'
-								onClick={toggleMenu}
-							>
-								<i className='ri-menu-line' />
-							</div>
-						</div>
-					</nav>
-				</div>
-			</header>
-			<Drawer open={open} title='Menu Principal' onClose={toggleMenu}>
-				<nav className='nav-mobile'>
-					<Link to='/'>HOME</Link>
-					<Link to='/sobre-nos'>SOBRE NÓS</Link>
-					<Link to='/cursos'>CURSOS</Link>
-					<Link to='/contato'>CONTATO</Link>
-				</nav>
-			</Drawer>
+              <div
+                className="button-mobile d-flex d-md-none"
+                onClick={toggleMenu}
+              >
+                <i className="ri-menu-line" />
+              </div>
+            </div>
+          </nav>
+        </div>
+      </header>
+      <Drawer open={open} title="Menu Principal" onClose={toggleMenu}>
+        <nav className="nav-mobile">
+          <Link to="/">HOME</Link>
+          <Link to="/sobre-nos">SOBRE NÓS</Link>
+          <Link to="/cursos">CURSOS</Link>
+          <Link to="/contato">CONTATO</Link>
+          <button className="button-default" onClick={toggleLogin}>
+            {labelAuth}
+          </button>
+        </nav>
+      </Drawer>
 
-			<Drawer
-				open={openLogin}
-				title={labelAuth}
-				onClose={toggleLogin}
-				footer={
-					profile?.data && (
-						<Button block danger onClick={handleLogout}>
-							Sair da Conta
-						</Button>
-					)
-				}
-			>
-				{profile?.data ? <FormProfile /> : <FormLogin />}
-			</Drawer>
-		</Spin>
-	);
+      <Drawer
+        open={openLogin}
+        title={labelAuth}
+        onClose={toggleLogin}
+        footer={
+          profile?.data && (
+            <Button block danger onClick={handleLogout}>
+              Sair da Conta
+            </Button>
+          )
+        }
+      >
+        {profile?.data ? <FormProfile /> : <FormLogin />}
+      </Drawer>
+    </Spin>
+  );
 };
